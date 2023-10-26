f82ef8cd36e54fea6ab79badf2327cf456f06545
termwq <145252136+termwq@users.noreply.github.com>
2023-10-26 13:20:41 +0300

    Add files via upload

---
+++ л2/3.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+// Для 10-ти чисел найти сумму элементов 
+// с одинаковым окончанием в троичной и пятеричной система счисления
+
+class same_end
+{
+    public static void Main()
+    {
+        int sum = 0;
+        for (int i = 0; i < 10; i++)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            if (n % 3 == n % 5)
+            {
+                sum += n;
+            }
+        }
+        Console.WriteLine("Сумма: {0}", sum);
+    }
+}
